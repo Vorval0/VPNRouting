08b9a6a39954ffa269d19ffa0b4d18984dc2f607
lavrov <lavrov@skbkontur.ru>
2022-04-07 01:57:13 +0500

    Теперь скрипт возвращает все сети для доменов

--- BuildRouting.sh
+++ BuildRouting.sh
@@ -1,4 +1,8 @@
 set -e
 set -o pipefail
 
-curl --fail -s https://api.bgpview.io/ip/13.107.42.14 | jq '.data.prefixes[].asn.asn' | xargs -I % curl --fail -s https://api.bgpview.io/asn/%/prefixes | jq -r '.data.ipv4_prefixes[].prefix' | aggregate -q
+echo $@ | \
+xargs dig +noall +answer -t A | awk '{print $NF}' | \
+xargs -I % curl --fail -s https://api.bgpview.io/ip/% | jq '.data.prefixes[].asn.asn' | \
+sort -u | xargs -I % curl --fail -s https://api.bgpview.io/asn/%/prefixes | jq -r '.data.ipv4_prefixes[].prefix' | \
+aggregate -q
