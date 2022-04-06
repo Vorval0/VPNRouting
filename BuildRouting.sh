bfe37f71a39923ef341c91db7bd86a3e79b49aa6
lavrov <lavrov@skbkontur.ru>
2022-04-06 18:45:52 +0500

    Буду запускать в wsl

--- BuildRouting.sh
+++ BuildRouting.sh
@@ -1,1 +1,4 @@
-curl -s https://api.bgpview.io/ip/13.107.42.14 | jq-win64.exe '.data.prefixes[].asn.asn' | tr -d '\r' | xargs -I % curl -s https://api.bgpview.io/asn/%/prefixes | jq-win64.exe '.data.ipv4_prefixes[].prefix'
+set -e
+set -o pipefail
+
+curl --fail -s https://api.bgpview.io/ip/13.107.42.14 | jq '.data.prefixes[].asn.asn' | xargs -I % curl --fail -s https://api.bgpview.io/asn/%/prefixes | jq '.data.ipv4_prefixes[].prefix'
