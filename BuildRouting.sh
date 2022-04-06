b1892354488b3288581e551568e6141dc582a78d
lavrov <lavrov@skbkontur.ru>
2022-04-06 19:01:18 +0500

    Аггрегируе префиксы

--- BuildRouting.sh
+++ BuildRouting.sh
@@ -1,4 +1,4 @@
 set -e
 set -o pipefail
 
-curl --fail -s https://api.bgpview.io/ip/13.107.42.14 | jq '.data.prefixes[].asn.asn' | xargs -I % curl --fail -s https://api.bgpview.io/asn/%/prefixes | jq '.data.ipv4_prefixes[].prefix'
+curl --fail -s https://api.bgpview.io/ip/13.107.42.14 | jq '.data.prefixes[].asn.asn' | xargs -I % curl --fail -s https://api.bgpview.io/asn/%/prefixes | jq -r '.data.ipv4_prefixes[].prefix' | aggregate -q
