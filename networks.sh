399d092cbaa6b5c46efeb0be74410d118e1d0867
lavrov <lavrov@skbkontur.ru>
2022-04-10 00:44:04 +0500

    abs.twimg.com иммет CNAME-запись, что приводило к проблемам в выводе dig

--- networks.sh
+++ networks.sh
@@ -2,7 +2,7 @@ set -e
 set -o pipefail
 
 echo $@ | \
-xargs dig +noall +answer -t A | awk '{print $NF}' | \
+xargs --no-run-if-empty --max-args=1 host -t A | awk '/has address/ { print $4 }' | \
 xargs -I % curl --fail -s https://api.bgpview.io/ip/% | jq '.data.prefixes[].asn.asn' | \
 sort -u | xargs -I % curl --fail -s https://api.bgpview.io/asn/%/prefixes | jq -r '.data.ipv4_prefixes[].prefix' | \
 aggregate -q
