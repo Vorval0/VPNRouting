dfe9ddfd8648270bfa5088bf49304263fc7673df
lavrov <lavrov@skbkontur.ru>
2022-04-27 01:28:24 +0500

    Ростелеком дублирует ip тик-тока, и нет смысла до его сетей маршрутизироваться черезе впн
    https://bgpview.io/ip/2.16.21.75

--- networks.sh
+++ networks.sh
@@ -3,6 +3,6 @@ set -o pipefail
 
 echo $@ | \
 xargs --no-run-if-empty --max-args=1 host -t A | awk '/has address/ { print $4 }' | \
-xargs -I % curl --fail -s https://api.bgpview.io/ip/% | jq '.data.prefixes[].asn.asn' | \
+xargs -I % curl --fail -s https://api.bgpview.io/ip/% | jq '.data.prefixes[].asn | select(.country_code != "RU") | .asn' | \
 sort -u | xargs -I % curl --fail -s https://api.bgpview.io/asn/%/prefixes | jq -r '.data.ipv4_prefixes[].prefix' | \
 aggregate -q
