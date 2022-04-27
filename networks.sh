d4030affa5d67ad2510c137a1dc0d5347c5535cf
lavrov <lavrov@skbkontur.ru>
2022-04-28 01:56:13 +0500

    Сетей меньше, чем /24 не бывает, т.ч. ускорим, уменьшив количество обрабатываемых сетей

--- networks.sh
+++ networks.sh
@@ -2,7 +2,7 @@ set -e
 set -o pipefail
 
 echo $@ | \
-xargs --no-run-if-empty --max-args=1 host -t A | awk '/has address/ { print $4 }' | \
-xargs -I % curl --fail -s https://api.bgpview.io/ip/% | jq '.data.prefixes[].asn | select(.country_code != "RU") | .asn' | \
+xargs --no-run-if-empty --max-args=1 host -t A | awk '/has address/ { print $4 }' | cut -d '.' -f 1-3 | sort -u | \
+xargs -I % curl --fail -s https://api.bgpview.io/ip/%.1 | jq '.data.prefixes[].asn | select(.country_code != "RU") | .asn' | \
 sort -u | xargs -I % curl --fail -s https://api.bgpview.io/asn/%/prefixes | jq -r '.data.ipv4_prefixes[].prefix' | \
 aggregate -q
