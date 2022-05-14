d4a0b644dec2e7de3c63fe4c3fac6039cc9a6216
Vorval_0 <vorval0@gmail.com>
2022-05-14 14:41:54 +0500

    рефакторинг: одна строка — одна задача

--- networks.sh
+++ networks.sh
@@ -3,6 +3,6 @@ set -o pipefail
 
 echo $@ | \
 xargs --no-run-if-empty --max-args=1 host -t A | awk '/has address/ { print $4 }' | cut -d '.' -f 1-3 | sort -u | \
-xargs -I % curl --fail -s https://api.bgpview.io/ip/%.1 | jq '.data.prefixes[].asn.asn' | \
-sort -u | xargs -I % curl --fail -s https://api.bgpview.io/asn/%/prefixes | jq -r '.data.ipv4_prefixes[] | select(.country_code != "RU") | .prefix' | \
+xargs -I % curl --fail -s https://api.bgpview.io/ip/%.1 | jq '.data.prefixes[].asn.asn' | sort -u | \
+xargs -I % curl --fail -s https://api.bgpview.io/asn/%/prefixes | jq -r '.data.ipv4_prefixes[] | select(.country_code != "RU") | .prefix' | \
 aggregate -q
