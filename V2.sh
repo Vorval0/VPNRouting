e1784e89f3a6b30af7a214499db8d8e7e2e70bef
Vorval0 <38347222+Vorval0@users.noreply.github.com>
2023-05-29 02:34:06 +0500

    Update V2.sh

--- V2.sh
+++ V2.sh
@@ -1,1 +1,1 @@
-curl --fail https://reestr.rublacklist.net/api/v3/ips/ | jq -r '.[]' | grep "\." | sed "s/\$/\/32/" | aggregate | awk '{print "ip route " $1 " wg0 auto"}' > /mnt/c/tmp/command.txt
+curl --fail https://reestr.rublacklist.net/api/v3/ips/ | jq -r '.[]' | grep "\." | sed "s/\$/\/32/" | aggregate | awk '{print "ip route " $1 " wg0 auto"}' | sed "s/\/32//" > /mnt/c/tmp/command.txt
